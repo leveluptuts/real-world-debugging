1b3dcaa97b222ae2c1801c7af844b39762f563f8
Scott Tolinski <scott.tolinski@gmail.com>
2019-06-13 12:47:36 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
+import hammy from './hammy.jpg';
 import './App.css';
 
 function App() {
+  const getDad = async () => {
+    const res = await fetch('https://icanhazdadjoke.co/', {
+      headers: {
+        Accept: 'application/json'
+      }
+    });
+    const data = await res.json();
+    console.log('data:', data);
+  };
+
+  useEffect(() => {
+    getDad();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={hammy} className="App-logo" alt="logo" />
+        <div className="offscreen" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
